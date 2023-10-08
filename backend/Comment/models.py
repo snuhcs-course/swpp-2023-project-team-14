4c94284b58b83be5b99313cfd6aa4e313db90588
daeguhighman <keroro19671967@gmail.com>
2023-10-08 16:34:50 +0900

    feat:backend modeling

--- backend/Comment/models.py
+++ backend/Comment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import PersonalUser
+from post.models import Post
 
 # Create your models here.
+class Comment(models.Model):
+  content = models.CharField
+  author = models.ForeignKey(PersonalUser, on_delete=models.CASCADE)
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  comment_like_users = models.ManyToManyField(PersonalUser, on_delete=models.CASCADE, related_name='comment_like_users', through='CommentLike')
+
+class CommentLike(models.Model):
+  user = models.ForeignKey(PersonalUser, on_delete=models.CASCADE)
+  comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
