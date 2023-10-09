56c6ef122faf7ddc04e1c91de5ac3e36d67fbf6b
Seongjun Jo <gukcvts679@snu.ac.kr>
2023-10-10 06:14:10 +0900

    feat: set up UI resources (drawable, font, color)

--- frontend/app/src/main/java/com/example/haengsha/ui/theme/Theme.kt
+++ frontend/app/src/main/java/com/example/haengsha/ui/theme/Theme.kt
@@ -41,7 +41,7 @@ private val LightColorScheme = lightColorScheme(
 fun HaengshaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -58,7 +58,7 @@ fun HaengshaTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
