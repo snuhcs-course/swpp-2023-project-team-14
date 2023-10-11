45742564b7637aac66bf74b80e47c6d005da165a
Jun-Young <toti010@naver.com>
2023-10-11 20:48:05 +0900

    - Add navigator and basic loginScreen.

--- frontend/app/src/main/java/com/example/haengsha/MainActivity.kt
+++ frontend/app/src/main/java/com/example/haengsha/MainActivity.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.haengsha.ui.screens.ScreenNavigate
 import com.example.haengsha.ui.theme.HaengshaTheme
-import com.example.haengsha.ui.uiComponents.ToastPreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ToastPreview()
+                    //ToastPreview()
+                    /* TODO HaengshaApp() */
+                    ScreenNavigate()
                 }
             }
         }
