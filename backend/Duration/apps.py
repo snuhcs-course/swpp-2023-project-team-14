5383c31ad654e82daf505aa1932ba626c78b30f8
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-12 00:34:07 +0900

    fixed configuration bugs related to models

--- backend/Duration/apps.py
+++ backend/Duration/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DurationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'duration'
+    name = 'Duration'
--- backend/haengsha/settings.py
+++ backend/haengsha/settings.py
@@ -44,11 +44,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post',
-    'user',
-    'favorite',
-    'duration',
-    'comment'
+    'Post.apps.PostConfig',
+    'User.apps.UserConfig',
+    'Favorite.apps.FavoriteConfig',
+    'Duration.apps.DurationConfig',
+    'Comment.apps.CommentConfig'
 ]
 
 MIDDLEWARE = [
@@ -96,7 +96,7 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = 'User.PersonalUser'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
