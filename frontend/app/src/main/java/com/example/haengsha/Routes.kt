3466a21f01c5e248bfb8e32f60355a41cbe1d2b1
Jun-Young <toti010@naver.com>
2023-10-12 00:37:19 +0900

    feat: LoginSreen enhanced & Navigation

---
+++ frontend/app/src/main/java/com/example/haengsha/Routes.kt
@@ -0,0 +1,9 @@
+package com.example.haengsha
+
+sealed class Routes(val route: String) {
+    object SignUp : Routes("SignUp")
+    object ForgotPassword : Routes("ForgotPassword")
+    object Login : Routes("Login")
+    object Dashboard : Routes("Dashboard")
+    object Home : Routes("Home")
+}
