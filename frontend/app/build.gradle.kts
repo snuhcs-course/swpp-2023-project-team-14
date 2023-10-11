089d3e140e62aa340aec9cbf4bd73b243d8a8425
daeguhighman <keroro19671967@gmail.com>
2023-10-11 21:30:02 +0900

    Revert "- Add navigator and basic loginScreen."
    This reverts commit 45742564b7637aac66bf74b80e47c6d005da165a.
    
    revert 45742564b7637aac66bf74b80e47c6d005da165a

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -68,11 +68,12 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.8.1")
 
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.navigation:navigation-compose:2.7.4")
 }
--- frontend/app/src/main/java/com/example/haengsha/ui/screens/SignupScreen.kt
+++ frontend/app/src/main/java/com/example/haengsha/ui/screens/SignupScreen.kt
@@ -2,17 +2,14 @@ package com.example.haengsha.ui.screens
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun SignupScreen(navController: NavHostController) {
-    val navController = rememberNavController()
+fun SignupScreen() {
 
 }
 
 @Preview(showBackground = true)
 @Composable
 fun SignupScreenPreview() {
-    SignupScreen(rememberNavController())
+    SignupScreen()
 }
