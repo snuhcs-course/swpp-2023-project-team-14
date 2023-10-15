077d710ad77c0582c4a8f878cba529621320b736
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-16 01:06:31 +0900

    signin api: added serializers

---
+++ backend/User/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path 
+from .views import signin 
+
+urlpatterns = [
+    path('signin/', signin, name='signin'),
+]
--- backend/User/views.py
+++ backend/User/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .serializers import UserSignInSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def signin(request):
+    serializer = UserSignInSerializer(data=request.data)
+    if serializer.is_valid():
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+        user = authenticate(username=email, password=password)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key,
+                            'message': 'The user has successfully logged in.'})
+        else:
+            return Response({'message': 'There is no account with the given email or password.'}, status=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
