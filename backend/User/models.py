65a0407da072217ed80fe564e572d32cb82b752b
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-17 21:15:26 +0900

    changes to models

--- backend/User/models.py
+++ backend/User/models.py
@@ -2,7 +2,6 @@ import uuid
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.forms import ChoiceField
 
 # Create your models here.
 class PersonalUser(AbstractUser):
