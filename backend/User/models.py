282746e5f8b90039a1370518104122e336229560
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-17 15:45:33 +0900

    updated user model to reflect frontend major, graade, interest

--- backend/User/models.py
+++ backend/User/models.py
@@ -6,8 +6,6 @@ from django.forms import ChoiceField
 
 # Create your models here.
 class PersonalUser(AbstractUser):
-  USER = 'User'
-  GROUP = 'Group'
   ROLE_CHOICES = (
     ('User', 'User'),
     ('Group', 'Group'),
@@ -18,10 +16,12 @@ class PersonalUser(AbstractUser):
     ('Art', '미술대학'),
     ('Education', '사범대학'),
     ('SocialSciences', '사회과학대학'),
+    ('Music', '음악대학'),
+    ('Humanities', '인문대학'),
+    ('NaturalSciences', '자연과학대학'),
   )
   GRADE_CHOICES = (
-    ('16', '16학번 이하'),
-    ('17', '17학번'),
+    ('17', '17학번 이상'),
     ('18', '18학번'),
     ('19', '19학번'),
     ('20', '20학번'),
@@ -30,18 +30,19 @@ class PersonalUser(AbstractUser):
     ('23', '23학번'),
   )
   INTEREST_CHOICES = (
+    ('dance', '댄스'),
+    ('meetup', '사교'),
+    ('social', '사회'),
+    ('theater', '연극'),
     ('music', '음악'),
-    ('dance', '음악'),
-    ('theater', '음악'),
-    ('social', '사교'),
     ('sports', '운동'),
-    ('religion', '종교'),
-    ('art', '예술')
+    ('art', '예술'),
+    ('religion', '종교')
   )
   nickname = models.CharField(max_length=10)
   email = models.EmailField()
   password = models.CharField(max_length=10)
-  role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=USER)
+  role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='User')
   major = models.CharField(max_length=20, choices=MAJOR_CHOICES, default='Business')
   grade = models.CharField(max_length=10, choices=GRADE_CHOICES, default='23')
   interest = models.CharField(max_length=10, choices=INTEREST_CHOICES, default='music')
