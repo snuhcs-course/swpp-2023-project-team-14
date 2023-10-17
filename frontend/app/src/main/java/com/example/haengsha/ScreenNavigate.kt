316070205ab26898d3a373b13d5255618fd3d9b3
Jun-Young <toti010@naver.com>
2023-10-17 14:01:50 +0900

    feat: FindPassword UI
    * UIs for finding password
    * Navigation tested

--- frontend/app/src/main/java/com/example/haengsha/ScreenNavigate.kt
+++ frontend/app/src/main/java/com/example/haengsha/ScreenNavigate.kt
@@ -1,11 +1,16 @@
 package com.example.haengsha
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.haengsha.ui.screens.HomeScreen
 import com.example.haengsha.ui.screens.LoginScreen
+import com.example.haengsha.ui.screens.findPassword.FindPasswordCompleteScreen
+import com.example.haengsha.ui.screens.findPassword.FindPasswordOrganizerScreen
+import com.example.haengsha.ui.screens.findPassword.FindPasswordScreen
+import com.example.haengsha.ui.screens.findPassword.PasswordResetScreen
 import com.example.haengsha.ui.screens.signup.SignupTypeScreen
 
 @Composable
@@ -21,11 +26,20 @@ fun ScreenNavigate() {
         composable(Routes.SignUp.route) {
             SignupTypeScreen(navController = navController)
         }
-        /*
-        composable(Routes.ForgotPassword.route) { navBackStack ->
-            ForgotPassword(navController = navController)
-        }
 
+        composable(Routes.FindPassword.route) { navBackStack ->
+            FindPasswordScreen(navController = navController, context = LocalContext.current)
+        }
+        composable(Routes.FindPasswordOrganization.route) { navBackStack ->
+            FindPasswordOrganizerScreen(navController = navController)
+        }
+        composable(Routes.PasswordReset.route) { navBackStack ->
+            PasswordResetScreen(navController = navController, context = LocalContext.current)
+        }
+        composable(Routes.FindPasswordComplete.route) { navBackStack ->
+            FindPasswordCompleteScreen(navController = navController)
+        }
+        /*
         composable(Routes.Dashboard.route) {
             Dashboard(navController = navController)
         }*/
@@ -38,7 +52,10 @@ fun ScreenNavigate() {
 
 sealed class Routes(val route: String) {
     object SignUp : Routes("SignUp")
-    object ForgotPassword : Routes("ForgotPassword")
+    object FindPassword : Routes("FindPassword")
+    object FindPasswordOrganization : Routes("FindPasswordOrganization")
+    object PasswordReset : Routes("PasswordReset")
+    object FindPasswordComplete : Routes("FindPasswordComplete")
     object Login : Routes("Login")
     object Dashboard : Routes("Dashboard")
     object Home : Routes("Home")
