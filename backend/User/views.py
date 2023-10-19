b8d88298c753ae5ae2f9cc698f510036e916d1ee
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-19 10:36:30 +0900

    changed HTTP request status from 401 to 400 for bad requests

--- backend/User/views.py
+++ backend/User/views.py
@@ -36,7 +36,7 @@ def signup(request):
     elif role == PersonalUser.GROUP:   
         return Response({'redirect': 'Group authentication page.'}, status=status.HTTP_200_OK)
     else:
-        return Response({'message': 'Role must be either User or Group.'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'message': 'Role must be either User or Group.'}, status=status.HTTP_400_BAD_REQUEST)
     
 def generate_unique_code():
     auth_code = uuid.uuid4()
@@ -54,7 +54,7 @@ def send_code_to_email(email, code):
 def verify_snu_email(request):
     email = request.data.get('email')
     if not email:
-        return Response({'message': 'The email field is required.'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'message': 'The email field is required.'}, status=status.HTTP_400_BAD_REQUEST)
     
     if PersonalUser.objects.filter(email=email):
         return Response({'message': 'A user account with the given email already exists.'}, status=status.HTTP_401_UNAUTHORIZED)
@@ -87,19 +87,19 @@ def verify_password(request):
     
     password = request.data.get('password')
     if len(password) < 4 or len(password) > 10:
-        return Response({'message': 'The password must have at least 4 characters and at most 10 characters.'}, status=status.HTTP_401_UNAUTHORIZED) 
+        return Response({'message': 'The password must have at least 4 characters and at most 10 characters.'}, status=status.HTTP_400_BAD_REQUEST) 
     if not password.isalnum():
-       return Response({'message': 'The password must consist of only alphanumeric characters.'}, status=status.HTTP_401_UNAUTHORIZED) 
+       return Response({'message': 'The password must consist of only alphanumeric characters.'}, status=status.HTTP_400_BAD_REQUEST) 
     if not contains_alpha(password):
-       return Response({'message': 'The password must contain at least 1 alphabetic character.'}, status=status.HTTP_401_UNAUTHORIZED)  
+       return Response({'message': 'The password must contain at least 1 alphabetic character.'}, status=status.HTTP_400_BAD_REQUEST)  
     if not contains_number(password):
-       return Response({'message': 'The password must contain at least 1 digit.'}, status=status.HTTP_401_UNAUTHORIZED) 
+       return Response({'message': 'The password must contain at least 1 digit.'}, status=status.HTTP_400_BAD_REQUEST) 
     
     password_confirm = request.data.get('password_confirm')
     if password == password_confirm:
         return Response({'message': 'Successfully created password.'}, status=status.HTTP_200_OK) 
     else:
-        return Response({'message': 'The password and the confirmation password do not match.'}, status=status.HTTP_401_UNAUTHORIZED) 
+        return Response({'message': 'The password and the confirmation password do not match.'}, status=status.HTTP_400_BAD_REQUEST) 
 
 
 @api_view(['POST'])
@@ -109,22 +109,22 @@ def create_profile(request):
     if length < 2 or length > 10:
         return Response({'message': 'The nickname must be at least 2 characters and at most 10 characters long.'}, status=status.HTTP_401_UNAUTHORIZED) 
     if PersonalUser.objects.filter(nickname=nickname):
-        return Response({'message': 'The nickname is already taken.'}, status=status.HTTP_401_UNAUTHORIZED) 
+        return Response({'message': 'The nickname is already taken.'}, status=status.HTTP_400_BAD_REQUEST) 
 
     major = request.data.get('major')
     valid_major_choices = [choice[0] for choice in PersonalUser.MAJOR_CHOICES]
     if not major in valid_major_choices:
-        return Response({'message': 'Invalid major.'}, status=status.HTTP_401_UNAUTHORIZED) 
+        return Response({'message': 'Invalid major.'}, status=status.HTTP_400_BAD_REQUEST) 
 
     grade = request.data.get('grade')
     valid_grade_choices = [choice[0] for choice in PersonalUser.GRADE_CHOICES]
     if not grade in valid_grade_choices:
-        return Response({'message': 'Invalid grade.'}, status=status.HTTP_401_UNAUTHORIZED) 
+        return Response({'message': 'Invalid grade.'}, status=status.HTTP_400_BAD_REQUEST) 
 
     interest = request.data.get('interest')
     valid_interest_choices = [choice[0] for choice in PersonalUser.INTEREST_CHOICES]
     if not interest in valid_interest_choices:
-        return Response({'message': 'Invalid interest.'}, status=status.HTTP_401_UNAUTHORIZED) 
+        return Response({'message': 'Invalid interest.'}, status=status.HTTP_400_BAD_REQUEST) 
 
     return Response({'message': 'Successfully finished inputting user info.'}, status=status.HTTP_200_OK) 
 
