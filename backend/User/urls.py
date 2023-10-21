5a5d9a3aef7c35a3ca81f0254c5d69e692625bf1
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-21 19:19:11 +0900

    added an api for nickname check

--- backend/User/urls.py
+++ backend/User/urls.py
@@ -4,6 +4,7 @@ from .views import (
     verify_snu_email,
     verify_code,
     verify_password,
+    check_nickname,
     signup
 )
 
@@ -12,5 +13,6 @@ urlpatterns = [
     path('api/verify_snu_email', verify_snu_email, name='verify_email'),
     path('api/verify_code', verify_code, name='verify_code'),
     path('api/verify_password', verify_password, name='verify_password'),
+    path('api/check_nickname', check_nickname, name='check_nickname'),
     path('api/signup', signup, name='signup')
 ]
