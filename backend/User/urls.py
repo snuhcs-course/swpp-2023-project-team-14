0daee0dc8f13aea2603e99086ca1ff18517c53f3
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-23 21:16:48 +0900

    updated change password

--- backend/User/urls.py
+++ backend/User/urls.py
@@ -4,7 +4,7 @@ from .views import (
     verify_snu_email_signup,
     verify_email_signin,
     verify_code,
-    verify_password,
+    change_password,
     check_nickname,
     signup
 )
@@ -13,7 +13,7 @@ urlpatterns = [
     path('api/signin', signin, name='signin'),
     path('api/verify_snu_email', verify_snu_email_signup, name='verify_email_signup'),
     path('api/verify_code', verify_code, name='verify_code_signup'),
-    path('api/verify_password', verify_password, name='verify_password'),
+    path('api/find/change_password', change_password, name='change_password'),
     path('api/check_nickname', check_nickname, name='check_nickname'),
     path('api/signup', signup, name='signup'),
     path('api/find/verify_email', verify_email_signin, name='verify_email_signin'),
