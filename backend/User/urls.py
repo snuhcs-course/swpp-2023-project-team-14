6723b5406a74ff76f6b12c9d0d249d5a5d4d67da
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-23 20:52:47 +0900

    added an api for password check while user is logged in

--- backend/User/urls.py
+++ backend/User/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
 from .views import (
     signin,
-    verify_snu_email,
+    verify_snu_email_signup,
+    verify_email_signin,
     verify_code,
     verify_password,
     check_nickname,
@@ -10,9 +11,11 @@ from .views import (
 
 urlpatterns = [
     path('api/signin', signin, name='signin'),
-    path('api/verify_snu_email', verify_snu_email, name='verify_email'),
-    path('api/verify_code', verify_code, name='verify_code'),
+    path('api/verify_snu_email', verify_snu_email_signup, name='verify_email_signup'),
+    path('api/verify_code', verify_code, name='verify_code_signup'),
     path('api/verify_password', verify_password, name='verify_password'),
     path('api/check_nickname', check_nickname, name='check_nickname'),
-    path('api/signup', signup, name='signup')
+    path('api/signup', signup, name='signup'),
+    path('api/find/verify_email', verify_email_signin, name='verify_email_signin'),
+    path('api/find/verify_code', verify_code, name='verify_code_signin'),
 ]
