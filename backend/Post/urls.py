1495ce77f3981356c18cced5888778038f7ca81e
daeguhighman <keroro19671967@gmail.com>
2023-10-24 18:21:51 +0900

    feat: post api 작업

--- backend/Post/urls.py
+++ backend/Post/urls.py
@@ -3,9 +3,36 @@ from .views import PostListView, PostDetailView, FavoriteView, LikeView, PostFav
 
 app_name = 'post'
 urlpatterns = [
+    path('', PostListView.as_view(), name='post-list'),
+
+    # 키워드 필터만 사용한 URL
+    path('keyword/<str:keyword>/', PostListView.as_view(), name='post-filter-by-keyword'),
+
+    # 축제 여부 필터만 사용한 URL
+    path('festival/<int:is_festival>/', PostListView.as_view(), name='post-filter-by-festival'),
+
+    # 날짜 범위 필터만 사용한 URL
+    path('date/<str:start_date>/<str:end_date>/', PostListView.as_view(), name='post-filter-by-date'),
+
+    # start_date만 사용한 URL
+    path('start_date/<str:start_date>/', PostListView.as_view(), name='post-filter-by-start-date'),
+
+    # end_date만 사용한 URL
+    path('end_date/<str:end_date>/', PostListView.as_view(), name='post-filter-by-end-date'),
+
+    # 키워드와 축제 여부 필터를 사용한 URL
+    path('keyword/<str:keyword>/festival/<int:is_festival>/', PostListView.as_view(), name='post-filter-keyword-and-festival'),
+
+    # 키워드와 날짌 범위 필터를 사용한 URL
+    path('keyword/<str:keyword>/date/<str:start_date>/<str:end_date>/', PostListView.as_view(), name='post-filter-keyword-and-date'),
+
+    # 축제 여부와 날짌 범위 필터를 사용한 URL
+    path('festival/<int:is_festival>/date/<str:start_date>/<str:end_date>/', PostListView.as_view(), name='post-filter-festival-and-date'),
+
+    # 모든 필터를 사용한 URL
+    path('keyword/<str:keyword>/festival/<int:is_festival>/date/<str:start_date>/<str:end_date>/', PostListView.as_view(), name='post-filter-all'),
     path('<int:post_id>/', PostDetailView.as_view()),
     path('favorite/<int:post_id>/', FavoriteView.as_view()),
     path('like/<int:post_id>/', LikeView.as_view()),
-    path('favorite/', PostFavoriteView.as_view()),
-    path('/<str:keyword>/<int:is_festival>/<str:start_date>/<str:end_date>/', PostListView.as_view()),
+    path('favorite/', PostFavoriteView.as_view())
 ]
--- backend/Post/views.py
+++ backend/Post/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from .models import Post,Like,Favorite,EventDuration,Duration
 from rest_framework.response import Response
 from .serializers import Postserializer
-from django.db.models import Count
+from django.db.models import Count, Q
 from datetime import date
 # Create your views here.
 # class TodayPostListView(APIView):
@@ -16,13 +16,24 @@ class PostListView(APIView):
   #   posts = Post.objects.all().annotate(like_count=Count('like_users')).order_by('-like_count')
   #   serializer = Postserializer(posts,many=True)
   #   return Response(serializer.data,status=200)
-  def get(self,request,keyword=None,is_festival=None,start_date=None,end_date=None):
+  def get(self, request, keyword=None, is_festival=None, start_date=None, end_date=None):
+    posts = Post.objects.all()
+
     if keyword:
-            posts = posts.filter(title__icontains=keyword)
+        posts = posts.filter(title__icontains=keyword)
     if is_festival is not None:
         posts = posts.filter(is_festival=is_festival)
+
     if start_date and end_date:
-        posts = posts.filter(event_durations__event_day__range=[start_date, end_date])
+        # start_date와 end_date가 모두 있을 경우
+        posts = posts.filter(Q(event_durations__event_day__range=[start_date, end_date]))
+
+    elif start_date:
+        # start_date만 있을 경우
+        posts = posts.filter(event_durations__event_day__gte=start_date)
+    elif end_date:
+        # end_date만 있을 경우
+        posts = posts.filter(event_durations__event_day__lte=end_date)
 
     posts = posts.annotate(like_count=Count('like_users')).order_by('-like_count')
 
