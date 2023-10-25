fe034a762aa364fe0f1f0f690e7ebcbcca426094
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-26 06:15:51 +0900

    added all original models

---
+++ backend/post/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from user.models import PersonalUser
+from duration.models import Duration
+# Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=30)
+  content = models.CharField
+  place = models.CharField(max_length=30)
+  image = models.ImageField
+  is_festival = models.BooleanField
+  like_users = models.ManyToManyField(PersonalUser, blank=True, related_name='like_posts', through='Like')
+  # interest_users = models.ManyToManyField(PersonalUser, blank=True, related_name='interest_posts', through='Interest')
+  event_duration = models.ManyToManyField(Duration, blank=True, related_name='duration_events', through='EventDuration')
+
+class Like(models.Model):
+  user = models.ForeignKey(PersonalUser, on_delete=models.CASCADE)
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  
+# class Interest(models.Model):
+#   user = models.ForeignKey(PersonalUser, on_delete=models.CASCADE)
+#   post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class EventDuration(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    duration = models.ForeignKey(Duration, on_delete=models.CASCADE)
