90c895fdc4d2c7b012861b5e2c89fdb50863d849
daeguhighman <keroro19671967@gmail.com>
2023-10-27 11:26:54 +0900

    serializer 수정

--- backend/Post/serializers.py
+++ backend/Post/serializers.py
@@ -1,26 +1,43 @@
 from rest_framework import serializers
-from .models import Post,Duration,EventDuration
+from .models import Post, Duration, EventDuration
 from user.models import PersonalUser
+
 
 class UsernicknameSerializer(serializers.ModelSerializer):
     class Meta:
         model = PersonalUser
-        fields = ["nickname",]
+        fields = [
+            "nickname",
+        ]
+
 
 class EventDurationSerializer(serializers.ModelSerializer):
-    event_day = serializers.DateField(source='duration.event_day')
+    event_day = serializers.DateField(source="duration.event_day")
 
     class Meta:
         model = EventDuration
-        fields = ('event_day')
+        fields = ["event_day"]
 
 
 class Postserializer(serializers.ModelSerializer):
     like_count = serializers.IntegerField()
     favorite_count = serializers.IntegerField()
-    event_duration = EventDurationSerializer(many=True, read_only=True, source='eventduration_set')
-    club = UsernicknameSerializer(read_only=True, source='author')
-    
+    event_durations = EventDurationSerializer(
+        many=True, read_only=True, source="eventduration_set"
+    )
+    club = UsernicknameSerializer(read_only=True, source="author")
+
     class Meta:
         model = Post
-        fields = ('title','content','place','image','is_festival','like_count','time','event_day','club','favorite_count')
+        fields = (
+            "title",
+            "content",
+            "place",
+            "image",
+            "is_festival",
+            "like_count",
+            "time",
+            "club",
+            "favorite_count",
+            "event_durations",
+        )
