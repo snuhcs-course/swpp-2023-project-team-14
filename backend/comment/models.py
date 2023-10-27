78f2fe3ffa2df7ca6675ebfe9e9af660de428641
daeguhighman <keroro19671967@gmail.com>
2023-10-27 12:02:39 +0900

    merge dev with other branchs

--- backend/comment/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from user.models import PersonalUser
-from post.models import Post
-
-# Create your models here.
-class Comment(models.Model):
-  content = models.CharField
-  author = models.ForeignKey(PersonalUser, on_delete=models.CASCADE)
-  post = models.ForeignKey(Post, on_delete=models.CASCADE)
