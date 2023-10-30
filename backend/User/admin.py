e987c62df14cecfedd05b44552546f08f169c151
Songmin17 <s.lee.0713@snu.ac.kr>
2023-10-30 14:18:04 +0900

    removed capitalized User model from github

--- backend/User/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import PersonalUser
-
-# Register your models here.
-admin.site.register(PersonalUser, UserAdmin)
