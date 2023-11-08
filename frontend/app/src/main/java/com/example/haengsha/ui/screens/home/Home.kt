3e7a8a03184f8a91f39bcd3154b8d20a9acea6a1
yuri-0415 <eileen0415@naver.com>
2023-11-09 02:48:42 +0900

    fix: add Date Picker

--- frontend/app/src/main/java/com/example/haengsha/ui/screens/home/Home.kt
+++ frontend/app/src/main/java/com/example/haengsha/ui/screens/home/Home.kt
@@ -3,6 +3,7 @@ package com.example.haengsha.ui.screens.home
 import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,10 +14,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -25,6 +32,7 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,6 +43,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.haengsha.R
 import com.example.haengsha.model.route.MainRoute
 import com.example.haengsha.model.uiState.UserUiState
 import com.example.haengsha.model.viewModel.event.EventViewModel
@@ -47,11 +56,14 @@ import com.kizitonwose.calendar.compose.weekcalendar.rememberWeekCalendarState
 import com.kizitonwose.calendar.core.atStartOfMonth
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
 import com.kizitonwose.calendar.core.yearMonth
+import kotlinx.coroutines.selects.select
+import java.text.SimpleDateFormat
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 import java.time.format.TextStyle
+import java.util.Date
 import java.util.Locale
 
 
@@ -121,8 +133,24 @@ fun HomeScreen(
         firstDayOfWeek = firstDayOfWeek
     )
 
+    var date by remember {
+        mutableStateOf("Open date picker dialog")
+    }
 
-    var isDatePickerDialogVisible by remember { mutableStateOf(false) }
+    var showDatePicker by remember {
+        mutableStateOf(false)
+    }
+
+    if (showDatePicker) {
+        MyDatePickerDialog(
+            onDateSelected = { selectedDate ->
+                selection = parseStringToDate(selectedDate)
+                showDatePicker = false // Close the date picker dialog
+            },
+            onDismiss = { showDatePicker = false }
+        )
+    }
+
 
     Column(
         modifier = Modifier
@@ -132,17 +160,38 @@ fun HomeScreen(
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(50.dp),
-        )
-        {
+                .height(50.dp)
+        ) {
+            // Center-aligned Text
+            Box(
+                modifier = Modifier
+                    .padding(start = 16.dp) // Add padding to the left of the Text
+                    .align(Alignment.Center) // Align the Text to the center-left
+            ) {
+                Text(
+                    text = monthFormatter.format(selection.month),
+                    fontFamily = poppins,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            }
 
-            Text(
-                modifier = Modifier.padding(top = 10.dp),
-                text = monthFormatter.format(selection.month),
-                fontFamily = poppins,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Medium
-            )
+            // Right-aligned Image
+            Box(
+                modifier = Modifier
+                    .padding(end = 16.dp) // Add padding to the right of the Image
+                    .align(Alignment.CenterEnd) // Align the Image to the center-right
+                    .clickable {
+                        showDatePicker = true
+                    }
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.calendar_icon),
+                    contentDescription = "calendar icon",
+                    modifier = Modifier.size(24.dp)
+
+                )
+            }
         }
 
         WeekCalendar(
@@ -151,23 +200,18 @@ fun HomeScreen(
             calendarScrollPaged = true,
             dayContent = { day ->
                 Day(day.date, isSelected = selection == day.date) { clicked ->
-
                     if (selection != clicked) {
                         selection = clicked
-                        eventViewModel.getEventByDate(eventType = "Academic", clicked)
-                        eventViewModel.getEventByDate(eventType = "Festival", clicked)
+                        eventViewModel.getEventByDate(eventType = "Academic", selection)
+                        eventViewModel.getEventByDate(eventType = "Festival", selection)
                     }
                 }
             },
         )
+
         TabView(sharedViewModel, selection, 0)
-        Button(
-            onClick = {
-                isDatePickerDialogVisible = true
-            }
-        ) {
-            Text("Pick Date")
-        }
+
+
     }
 }
 
@@ -177,6 +221,16 @@ private val dateFormatter = DateTimeFormatter.ofPattern("dd")
 
 @RequiresApi(Build.VERSION_CODES.O)
 private val monthFormatter = DateTimeFormatter.ofPattern("MMMM")
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun parseStringToDate(dateString: String): LocalDate? {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    return try {
+        LocalDate.parse(dateString, formatter)
+    } catch (e: Exception) {
+        null // Handle the case when the input string is not in the expected format
+    }
+}
 
 @RequiresApi(Build.VERSION_CODES.O)
 fun DayOfWeek.displayText(uppercase: Boolean = false): String {
@@ -223,6 +277,48 @@ private fun Day(date: LocalDate, isSelected: Boolean, onClick: (LocalDate) -> Un
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyDatePickerDialog(
+    onDateSelected: (String) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    val selectedDate = datePickerState.selectedDateMillis?.let {
+        convertMillisToDate(it)
+    } ?: ""
+
+    DatePickerDialog(
+        onDismissRequest = { onDismiss() },
+        confirmButton = {
+            Button(onClick = {
+                onDateSelected(selectedDate)
+                onDismiss()
+            }
+
+            ) {
+                Text(text = "OK")
+            }
+        },
+        dismissButton = {
+            Button(onClick = {
+                onDismiss()
+            }) {
+                Text(text = "Cancel")
+            }
+        }
+    ) {
+        DatePicker(
+            state = datePickerState
+        )
+    }
+}
+
+private fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("yyyy-MM-dd")
+    return formatter.format(Date(millis))
+}
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Home(
