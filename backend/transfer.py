8a1fd4a59cf2dce6194d7ab827ba264ffe6eada4
Jun-Young <toti010@naver.com>
2023-11-09 00:54:11 +0900

    feat: transfer data for ML inference
    * Add pipeline for extracting machine input for user data
    * Add data directory to collect data from db.

--- backend/transfer.py
+++ backend/transfer.py
@@ -8,15 +8,47 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'haengsha.settings')
 django.setup()
 
 
-
-from post.models import Post
 from user.models import PersonalUser
+from post.models import Post, Like, Favorite
+from datetime import date
 
-def get_data():
-    myset = PersonalUser.objects.filter(role='User')
-    myset_list = list(myset)
-    print(myset_list)
-    return myset_list 
+def save_userdata(save_dir, filename='userData.csv'):
+    users = PersonalUser.objects.all()
+    user_data = []
+
+    for user in users:
+        liked_posts = Post.objects.filter(like__user=user).values('title', 'is_festival')
+        favorite_posts = Post.objects.filter(favorite__user=user).values('title', 'is_festival')
+
+        liked_academic_posts = [post['title'] for post in liked_posts if not post['is_festival']]
+        liked_festival_posts = [post['title'] for post in liked_posts if post['is_festival']]
+        favorite_academic_posts = [post['title'] for post in favorite_posts if not post['is_festival']]
+        favorite_festival_posts = [post['title'] for post in favorite_posts if post['is_festival']]
+
+        # Concatenate the liked and favorite posts into single strings
+        liked_academic_concat = ', '.join(liked_academic_posts)
+        liked_festival_concat = ', '.join(liked_festival_posts)
+        favorite_academic_concat = ', '.join(favorite_academic_posts)
+        favorite_festival_concat = ', '.join(favorite_festival_posts)
+
+        user_info = {
+            'user_id': user.id,
+            '소속학과': user.major,
+            '학번': user.grade,  
+            '관심취미': user.interest,
+            '좋아요한 학술 행사': liked_academic_concat,
+            '좋아요한 공연 행사': liked_festival_concat,
+            '관심있는 학술 행사': favorite_academic_concat,
+            '관심있는 공연 행사': favorite_festival_concat,
+            '학술 행사의 선호도': len(liked_academic_posts)+len(favorite_academic_posts),
+            '공연 행사의 선호도': len(liked_festival_posts)+len(favorite_festival_posts)  
+        }
+
+        user_data.append(user_info)
+
+    user_df = pd.DataFrame(user_data)
+    user_df.to_csv(save_dir+filename, index=False)
+    return filename
 
 def put_data():
     file_name = '../recommend/all_recommends.csv'
@@ -25,4 +57,8 @@ def put_data():
     posts = Post.objects.all()
     
 
-get_data()
+if __name__ == "__main__":
+    save_dir = "./data/"
+    ##read_dir = "./machine_output_data/"
+    
+    save_userdata(save_dir)
