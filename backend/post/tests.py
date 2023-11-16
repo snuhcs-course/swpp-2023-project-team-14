65d5fe1364c68e414e300b479bdd4693e637fe9c
daeguhighman <keroro19671967@gmail.com>
2023-11-16 16:58:24 +0900

    post.test

--- backend/post/tests.py
+++ backend/post/tests.py
@@ -7,21 +7,79 @@ from post.models import Post
 # Create your tests here.
 class RegisterPostAPITestCase(APITestCase):
     def setUp(self):
-        self.user_data = {'email':'test@snu.ac.kr','password':'test1234'}
+        self.user_data = {'email':'test@snu.ac.kr','password':'test1234','role':'Group'}
+        self.post_data = {"title": "test", "content": "test", "place": "test", "image": "test", "is_festival": 0, "time": "test", "duration": [{"event_day": "2023-10-23"},{"event_day": "2023-10-24"},{"event_day": "2023-10-27"},{"event_day": "2023-10-28"}]}
+        self.user = PersonalUser.objects.create_user(**self.user_data)
+        self.token = self.client.post(reverse('signin'),self.user_data, format='json').data['token']
+    ## Test for register post
+    def test_register_post(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.post(reverse('post:post-list'),self.post_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class GetPostListAPITestCase(APITestCase):
+    def setUp(self):
+        self.user_data = {'email':'test@snu.ac.kr','password':'test1234','role':'Group'}
+        self.post_data = {"title": "test", "content": "test", "place": "test", "image": "test", "is_festival": 0, "time": "test", "duration": [{"event_day": "2023-10-23"},{"event_day": "2023-10-24"},{"event_day": "2023-10-27"},{"event_day": "2023-10-28"}]}
+        self.post_data_2 = {"title": "test2", "content": "test2", "place": "test2", "image": "test2", "is_festival": 0, "time": "test2", "duration": [{"event_day": "2023-10-23"},{"event_day": "2023-10-24"},{"event_day": "2023-10-27"},{"event_day": "2023-10-28"}]}
+        self.user = PersonalUser.objects.create_user(**self.user_data)
+        self.token = self.client.post(reverse('signin'),self.user_data).data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        self.post_1 = self.client.post(reverse('post:post-list'),self.post_data, format='json').data
+        self.post_2 = self.client.post(reverse('post:post-list'),self.post_data_2, format='json').data
+
+    def test_get_post(self):
+        response = self.client.get(reverse('post:post-list'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class GetPostDetailAPITestCase(APITestCase):
+    def setUp(self):
+        self.user_data = {'email':'test@snu.ac.kr','password':'test1234','role':'Group'}
         self.post_data = {"title": "test", "content": "test", "place": "test", "image": "test", "is_festival": 0, "time": "test", "duration": [{"event_day": "2023-10-23"},{"event_day": "2023-10-24"},{"event_day": "2023-10-27"},{"event_day": "2023-10-28"}]}
         self.user = PersonalUser.objects.create_user(**self.user_data)
         self.token = self.client.post(reverse('signin'),self.user_data).data['token']
-    # def test_get_token(self):
-    #     response = self.client.post(reverse('signin'),self.user_data)
-    #     print(response.data['token'])
-    #     self.assertEqual(response.status_code,status.HTTP_200_OK)
-    def test_register_post(self):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
-        print(self.post_data)
-        response = self.client.post(reverse('post:post-list'),self.post_data)
-        # self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+        self.post = self.client.post(reverse('post:post-list'),self.post_data, format='json').data
+        self.post_id = self.post.get('id')
+        
+
+    def test_get_detail_post(self):       
+        response = self.client.get(reverse('post:post-detail', kwargs={'post_id':self.post_id}), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_post(self):
+        response = self.client.delete(reverse('post:post-detail', kwargs={'post_id':self.post_id}), format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+class LikeorFavoritePostAPITestCase(APITestCase):
+    def setUp(self):
+        self.user_data = {'email':'test@snu.ac.kr','password':'test1234','role':'Group'}
+        self.post_data = {"title": "test", "content": "test", "place": "test", "image": "test", "is_festival": 0, "time": "test", "duration": [{"event_day": "2023-10-23"},{"event_day": "2023-10-24"},{"event_day": "2023-10-27"},{"event_day": "2023-10-28"}]}
+        self.user = PersonalUser.objects.create_user(**self.user_data)
+        self.token = self.client.post(reverse('signin'),self.user_data).data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        self.post = self.client.post(reverse('post:post-list'),self.post_data, format='json').data
+        self.post_id = self.post.get('id')
     
-    # def test_fail_if_not_logged_in(self):
-    #     response = self.client.post(reverse('post:post-list'),self.post_data)
-    #     self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
-    
+    def test_like_post(self):
+        response = self.client.patch(reverse('post:post-like', kwargs={'post_id':self.post_id}), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_favorite_post(self):
+        response = self.client.patch(reverse('post:post-favorite', kwargs={'post_id':self.post_id}), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class RecommendPostAPITestCase(APITestCase):
+    def setUp(self):
+        self.user_data = {'email':'test@snu.ac.kr','password':'test1234','role':'Group'}
+        self.post_data = {"title": "test", "content": "test", "place": "test", "image": "test", "is_festival": 0, "time": "test", "duration": [{"event_day": "2023-10-23"},{"event_day": "2023-10-24"},{"event_day": "2023-10-27"},{"event_day": "2023-10-28"}]}
+        self.user = PersonalUser.objects.create_user(**self.user_data)
+        self.token = self.client.post(reverse('signin'),self.user_data).data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        self.post = self.client.post(reverse('post:post-list'),self.post_data, format='json').data
+        self.post_id = self.post.get('id')
+
+
+
+        
+        
