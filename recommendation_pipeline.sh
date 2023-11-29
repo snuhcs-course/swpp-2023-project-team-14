4f2e630bc62d453eece0b62a8fdb5cbf15f4fc86
Songmin17 <s.lee.0713@snu.ac.kr>
2023-11-29 14:14:45 +0900

    error-free version of ml routine

--- recommendation_pipeline.sh
+++ recommendation_pipeline.sh
@@ -30,10 +30,10 @@ echo \"[\$current_timestamp] Start copying userData & eventData from shared volu
 VOLUME_PATH=\"/mlvolume/\"; \
 USER_FILE="userData.csv"; \
 EVENT_FILE="eventData.csv"; \
-USER_DST_PATH=\"/app/data/\$USER_FILE\"; \
-EVENT_DST_PATH=\"/app/data/\$EVENT_FILE\"; \
+USER_DST_PATH=\"/app/data/\"; \
+EVENT_DST_PATH=\"/app/data/\"; \
 RECOMMENDATION_FILE="all_recommends_index.csv"; \
-RECOMMENDATION_PATH=\"/app/\$RECOMMENDATION_FILE\"; \
+RECOMMENDATION_PATH=\"/app/data/\$RECOMMENDATION_FILE\"; \
 cp \$VOLUME_PATH\$USER_FILE \$USER_DST_PATH; \
 cp \$VOLUME_PATH\$EVENT_FILE \$EVENT_DST_PATH; \
 current_timestamp=\$(date +'%Y-%m-%d %H:%M:%S'); \
@@ -46,17 +46,15 @@ python /app/recommend.py; \
 cp \$RECOMMENDATION_PATH \$VOLUME_PATH;"
 
 # # go into the backend container and do the third part
-# current_timestamp=\$(date +"%Y-%m-%d %H:%M:%S")
-# echo "[$current_timestamp] Connecting to the backend container..."
-
-# docker exec haeng bash -c "current_time=\$(TZ=Asia/Seoul date +%H:%M) \
-# current_timestamp=\$(date +"%Y-%m-%d %H:%M:%S") \
-# echo \"[\$current_timestamp] Copying the recommendation results into the container...\" \
-# RECOMMENDATION_FILE="all_recommends_index.csv" \
-# VOLUME_PATH=\"/haengvolume\"; \
-# RECOMMENDATION_DST_PATH="/app/data/\$RECOMMENDATION_FILE" \
-# cp \$VOLUME_PATH\$RECOMMENDATION_FILE \$RECOMMENDATION_DST_PATH \
-# echo \"[\$current_timestamp] Delete redundant recommendations and load to DB...\" \
-# python3 transfer.py -l \
-# echo \"[\$current_timestamp] Finished!\" "
-
+current_timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+echo "[$current_timestamp] Connecting to the backend container..."
+docker exec haeng bash -c "current_time=\$(TZ=Asia/Seoul date +%H:%M); \
+current_timestamp=\$(date +'%Y-%m-%d %H:%M:%S'); \
+echo \"[\$current_timestamp] Copying the recommendation results into the container...\"; \
+RECOMMENDATION_FILE="all_recommends_index.csv"; \
+VOLUME_PATH=\"/haengvolume/\"; \
+RECOMMENDATION_DST_PATH="/app/data/"; \
+cp \$VOLUME_PATH/\$RECOMMENDATION_FILE \$RECOMMENDATION_DST_PATH; \
+echo \"[\$current_timestamp] Delete redundant recommendations and load to DB...\"; \
+python transfer.py -l; \
+echo \"[\$current_timestamp] Finished!\"; "
