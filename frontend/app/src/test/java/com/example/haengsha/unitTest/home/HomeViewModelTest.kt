30182235457d94c1228d7c4e512091e44dd91693
Seongjun Jo <gukcvts679@snu.ac.kr>
2023-12-06 23:40:49 +0900

    test: add home view model unit test
    * Left as annotation due to unresolved error

---
+++ frontend/app/src/test/java/com/example/haengsha/unitTest/home/HomeViewModelTest.kt
@@ -0,0 +1,33 @@
+package com.example.haengsha.unitTest.home
+/*
+import com.example.haengsha.model.viewModel.home.HomeViewModel
+import com.example.haengsha.ui.screens.home.EventCardData
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import java.time.LocalDate
+
+class HomeViewModelTest {
+    private var viewModel = HomeViewModel()
+
+    private val startDate = LocalDate.of(2023, 12, 25)
+    private val endDate = LocalDate.of(2023, 12, 31)
+
+
+    @Test
+    fun homeViewModel_updateEventItems_updateIsCorrect() {
+        val festivalItems = listOf(
+            EventCardData("organizer1", "title1", startDate, endDate, 1, 1, "festival"),
+            EventCardData("organizer2", "title2", startDate, startDate, 1, 1, "festival")
+        )
+        val academicItems = listOf(
+            EventCardData("organizer3", "title3", startDate, endDate, 1, 1, "academic"),
+            EventCardData("organizer4", "title4", startDate, startDate, 1, 1, "academic")
+        )
+        TODO NullPointerException occurs
+        viewModel.updateEventItems(festivalItems, academicItems)
+
+        assertEquals(viewModel.festivalItems.value, festivalItems)
+        assertEquals(viewModel.academicItems.value, academicItems)
+    }
+}
+*/
